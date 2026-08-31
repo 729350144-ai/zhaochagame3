5010ab08f9a487aa2ffa4320f0c58d6adc9bd974
729350144-ai <729350144@qq.com>
2026-08-31 19:31:25 +0800

    上传游戏

---
+++ src/settings.51c5e.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/level0/关卡选择.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"4bb49",main:"3aa46"}};
